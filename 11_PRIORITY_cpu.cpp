97811463cdbc88820ea7beb25d462a1a2164aa23
ranaji25 <154081733+ranaji25@users.noreply.github.com>
2024-11-15 09:16:31 +0530

    Update 11_PRIORITY_cpu.cpp

--- 11_PRIORITY_cpu.cpp
+++ 11_PRIORITY_cpu.cpp
@@ -31,13 +31,21 @@ void preemptivePriorityScheduling(vector<Process>& processes, int n) {
     vector<int> waitingTime(n, 0);
     vector<int> turnaroundTime(n, 0);
     vector<int> finishTime(n, 0);
+    vector<int> remainingBurstTime(n); // To track remaining burst time of processes
+    vector<bool> isInQueue(n, false); // To track if a process is already in the queue
     int completed = 0;
 
+    // Initialize remaining burst time with the initial burst times
+    for (int i = 0; i < n; i++) {
+        remainingBurstTime[i] = processes[i].burst_time;
+    }
+
     while (completed < n) {
-        // Add processes that have arrived
+        // Add processes that have arrived and are not in the queue
         for (int i = 0; i < n; i++) {
-            if (processes[i].arrival_time <= currentTime && processes[i].burst_time > 0) {
+            if (processes[i].arrival_time <= currentTime && remainingBurstTime[i] > 0 && !isInQueue[i]) {
                 readyQueue.push(processes[i]);
+                isInQueue[i] = true; // Mark the process as added to the queue
             }
         }
 
@@ -46,43 +54,35 @@ void preemptivePriorityScheduling(vector<Process>& processes, int n) {
             Process current = readyQueue.top();
             readyQueue.pop();
 
-            int remainingTime = current.burst_time;
-            if (remainingTime > 1) {
-                current.burst_time = remainingTime - 1;
-                readyQueue.push(current);
-            }
-
             int processId = current.id - 1;
-            if (finishTime[processId] == 0) {
-                finishTime[processId] = currentTime + 1;
+
+            // Execute the process for one unit of time
+            remainingBurstTime[processId]--;
+            currentTime++;
+
+            // If the process is completed
+            if (remainingBurstTime[processId] == 0) {
+                finishTime[processId] = currentTime;
                 waitingTime[processId] = finishTime[processId] - processes[processId].arrival_time - processes[processId].burst_time;
                 turnaroundTime[processId] = finishTime[processId] - processes[processId].arrival_time;
                 completed++;
+            } else {
+                // Push the process back to the queue with reduced burst time
+                readyQueue.push(current);
             }
-            currentTime++;
         } else {
-            currentTime++;
+            currentTime++; // If no process is ready, just increment time
         }
     }
 
     // Display results
-    cout << "Gantt Chart:\n";
-    for (const Process& process : processes) {
-        cout << "| P" << process.id << " ";
-    }
-    cout << "|\n";
-    cout << "0 ";
-    for (int time : finishTime) {
-        cout << " " << time << " ";
-    }
-    cout << endl;
-
     cout << "Process ID\tArrival Time\tBurst Time\tPriority\tWaiting Time\tTurnaround Time\tFinish Time\n";
     for (int i = 0; i < n; i++) {
         cout << processes[i].id << "\t\t" << processes[i].arrival_time << "\t\t" << processes[i].burst_time << "\t\t" 
              << processes[i].priority << "\t\t" << waitingTime[i] << "\t\t" << turnaroundTime[i] << "\t\t" << finishTime[i] << endl;
     }
 }
+
 
 // Function for Non-Preemptive Priority Scheduling
 void nonPreemptivePriorityScheduling(vector<Process>& processes, int n) {
@@ -161,3 +161,51 @@ int main() {
 
     return 0;
 }
+
+/*
+Enter the number of processes: 7
+Enter arrival time for process 1: 0
+Enter burst time for process 1: 3
+Enter priority for process 1: 2
+Enter arrival time for process 2: 2
+Enter burst time for process 2: 5
+Enter priority for process 2: 6
+Enter arrival time for process 3: 1
+Enter burst time for process 3: 4
+Enter priority for process 3: 3
+Enter arrival time for process 4: 4
+Enter burst time for process 4: 2
+Enter priority for process 4: 5
+Enter arrival time for process 5: 6
+Enter burst time for process 5: 9
+Enter priority for process 5: 7
+Enter arrival time for process 6: 5
+Enter burst time for process 6: 4
+Enter priority for process 6: 4
+Enter arrival time for process 7: 7
+Enter burst time for process 7: 10
+Enter priority for process 7: 10
+
+Preemptive Priority Scheduling
+Process ID      Arrival Time    Burst Time      Priority        Waiting Time    Turnaround Time Finish Time
+1               0               3               2               0               3               3
+3               1               4               3               18              22              23
+2               2               5               6               1               6               8
+4               4               2               5               13              15              19
+6               5               4               4               18              22              27
+5               6               9               7               2               11              17
+7               7               10              10              20              30              37
+
+Non-Preemptive Priority Scheduling
+Gantt Chart:
+| P7           | P5          | P2      | P4   | P6     | P3     | P1    |
+0  10  19  24  26  30  34  37
+Process ID      Arrival Time    Burst Time      Priority        Turnaround Time Finish Time     Waiting Time
+7               7               10              10              3               10              -7
+5               6               9               7               13              19              4
+2               2               5               6               22              24              17
+4               4               2               5               22              26              20
+6               5               4               4               25              30              21
+3               1               4               3               33              34              29
+1               0               3               2               37              37              34
+*/
