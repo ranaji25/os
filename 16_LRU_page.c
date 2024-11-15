63b2a6acc4c7ae75a0fd822915bbcce8d471f5e8
ranaji25 <154081733+ranaji25@users.noreply.github.com>
2024-11-15 10:06:24 +0530

    Update 16_LRU_page.c

--- 16_LRU_page.c
+++ 16_LRU_page.c
@@ -1,69 +1,86 @@
 #include <stdio.h>
-#define MAX_PAGES 3
+#define MAX_FRAMES 3
+
+// Function to check if the page is in memory
 int isInMemory(int page, int* memory, int numFrames) {
-for (int i = 0; i < numFrames; i++) {
-if (memory[i] == page) {
-return 1; // Page is in memory
+    for (int i = 0; i < numFrames; i++) {
+        if (memory[i] == page) {
+            return i; // Return the frame index if page is found
+        }
+    }
+    return -1; // Page not found
 }
-}
-return 0; // Page is not in memory
-}
+
+// Function to display the current state of memory
 void displayMemory(int* memory, int numFrames) {
-printf("Memory: ");
-for (int i = 0; i < numFrames; i++) {
-printf("%d ", memory[i]);
+    printf("Memory: ");
+    for (int i = 0; i < numFrames; i++) {
+        if (memory[i] == -1) {
+            printf("- "); // Placeholder for empty slots
+        } else {
+            printf("%d ", memory[i]);
+        }
+    }
+    printf("\n");
 }
-printf("\n");
-}
+
+// Function to update LRU counters
 void updateLRU(int* lru, int numFrames, int accessedFrame) {
-for (int i = 0; i < numFrames; i++) {
-if (lru[i] < lru[accessedFrame]) {
-lru[i]++;
+    for (int i = 0; i < numFrames; i++) {
+        lru[i]++;
+    }
+    lru[accessedFrame] = 0; // Reset the counter for the accessed frame
 }
-}
-lru[accessedFrame] = 0;
-}
+
+// LRU page replacement algorithm
 void LRU(int* pages, int numPages) {
-int memory[MAX_PAGES] = { -1 };
-int lru[MAX_PAGES] = { 0 };
-int hits = 0, misses = 0;
-printf("Referenced String: ");
-for (int i = 0; i < numPages; i++) {
-printf("%d ", pages[i]);
+    int memory[MAX_FRAMES] = { -1, -1, -1 };  // Initialize memory with -1 (empty)
+    int lru[MAX_FRAMES] = { 0 };              // LRU counters for each frame
+    int hits = 0, misses = 0;
+
+    // Print the referenced page string
+    printf("Referenced String: ");
+    for (int i = 0; i < numPages; i++) {
+        printf("%d ", pages[i]);
+    }
+    printf("\n");
+
+    printf("LRU Page Replacement:\n");
+    for (int i = 0; i < numPages; i++) {
+        int currentPage = pages[i];
+        int accessedFrame = isInMemory(currentPage, memory, MAX_FRAMES);
+
+        if (accessedFrame != -1) {
+            // If the page is in memory, it's a hit
+            hits++;
+            updateLRU(lru, MAX_FRAMES, accessedFrame);
+        } else {
+            // Page miss, replace the least recently used page
+            misses++;
+
+            // Find the frame with the largest LRU counter (least recently used)
+            int replaceIndex = 0;
+            for (int j = 1; j < MAX_FRAMES; j++) {
+                if (lru[j] > lru[replaceIndex]) {
+                    replaceIndex = j;
+                }
+            }
+            memory[replaceIndex] = currentPage; // Replace the least recently used page
+            updateLRU(lru, MAX_FRAMES, replaceIndex); // Update LRU after replacement
+        }
+
+        // Display the current state of memory
+        displayMemory(memory, MAX_FRAMES);
+    }
+
+    // Print the results
+    printf("Hits: %d\n", hits);
+    printf("Misses: %d\n", misses);
+    printf("Page Faults: %d\n", misses);  // Misses and page faults are the same
 }
-printf("\n");
-printf("LRU Page Replacement:\n");
-for (int i = 0; i < numPages; i++) {
-int currentPage = pages[i];
-if (isInMemory(currentPage, memory, MAX_PAGES)) {
-hits++;
-int accessedFrame = -1;
-for (int j = 0; j < MAX_PAGES; j++) {
-if (memory[j] == currentPage) {
-accessedFrame = j;
-break;
-}
-}
-updateLRU(lru, MAX_PAGES, accessedFrame);
-} else {
-misses++;
-int replaceIndex = 0;
-for (int j = 1; j < MAX_PAGES; j++) {
-if (lru[j] > lru[replaceIndex]) {
-replaceIndex = j;
-}
-}
-memory[replaceIndex] = currentPage;
-updateLRU(lru, MAX_PAGES, replaceIndex);
-}
-displayMemory(memory, MAX_PAGES);
-}
-printf("Hits: %d\n", hits);
-printf("Misses: %d\n", misses);
-printf("Page Faults: %d\n", misses);
-}
+
 int main() {
-int numPages;
+    int numPages;
 
     // Get the number of pages
     printf("Enter the number of pages: ");
@@ -77,8 +94,47 @@ int numPages;
         scanf("%d", &pages[i]);
     }
 
-// int pages[] = {1, 2, 3, 4, 2, 1, 5, 6, 2, 1, 2, 3, 6};
-// int numPages = sizeof(pages) / sizeof(pages[0]);
-LRU(pages, numPages);
-return 0;
+    LRU(pages, numPages);
+    return 0;
 }
+
+/*
+Enter the number of pages: 15
+Enter the page references:
+7  
+0
+1
+2
+0
+3
+0
+4
+2
+3
+0
+3
+1
+2
+0
+Referenced String: 7 0 1 2 0 3 0 4 2 3 0 3 1 2 0
+LRU Page Replacement:
+Memory: 7 - -
+Memory: 7 0 -
+Memory: 7 0 1
+Memory: 2 0 1
+Memory: 2 0 1
+Memory: 2 0 3
+Memory: 2 0 3
+Memory: 4 0 3
+Memory: 4 0 2
+Memory: 4 3 2
+Memory: 0 3 2
+Memory: 0 3 2
+Memory: 0 3 1
+Memory: 2 3 1
+Memory: 2 0 1
+Hits: 3
+Misses: 12
+Page Faults: 12
+PS E:\_.sem5\OS\lab ex> 
+*/
