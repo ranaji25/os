494cf5ed4e80dd4214dddf8823c8802358875733
ranaji25 <154081733+ranaji25@users.noreply.github.com>
2024-11-15 10:34:02 +0530

    Update 18_FCFS_disk.c

--- 18_FCFS_disk.c
+++ 18_FCFS_disk.c
@@ -39,3 +39,29 @@ int main() {
     fcfs(requestQueue, n, head);
     return 0;
 }
+
+
+/*
+Enter the number of requests: 9
+Enter the request queue:
+55 58 39 18 90 160 150 38 184
+Enter the initial head position: 100
+FCFS Disk Scheduling Algorithm
++------------------------+------------------+------------------+
+| Request                | Head Movement     | Cumulative Seek  |
++------------------------+------------------+------------------+
+| 100                    -> 55     | 45               | 45               |
+| 55                     -> 58     | 3                | 48               |
+| 58                     -> 39     | 19               | 67               |
+| 39                     -> 18     | 21               | 88               |
+| 18                     -> 90     | 72               | 160              |
+| 90                     -> 160    | 70               | 230              |
+| 160                    -> 150    | 10               | 240              |
+| 150                    -> 38     | 112              | 352              |
+| 38                     -> 184    | 146              | 498              |
++------------------------+------------------+------------------+
+Total Seek Time (FCFS): 498
+
+PS E:\_.sem5\OS\lab ex>
+*/
+
